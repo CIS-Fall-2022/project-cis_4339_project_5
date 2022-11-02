149a4baa0f1c95ad027cefc9eb35df83c67c0177
lmdang3 <89552436+lmdang3@users.noreply.github.com>
2022-11-01 22:31:37 -0500

    created second instance test

---
+++ backend-B/models/models.js
@@ -0,0 +1,123 @@
+const uuid = require('uuid');
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//collection for intakeData for clients includes the embedded address object schema
+let primaryDataSchema = new Schema({
+    _id: { type: String, default: uuid.v1 },
+    client_id: {
+        type: Number,
+        required: true,
+        unique: true
+    },
+    firstName: {
+        type: String,
+        require: true
+    },
+    middleName: {
+        type: String,
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+
+    org_id:{
+        type: mongoose.Schema.Types.Number, // or ObjectID
+        ref: 'organization'
+        
+    },
+    email: {
+        type: String
+    },
+    phoneNumbers: [{
+        type: Number,
+        required: true
+    }],
+    address: {
+        line1: {
+            type: String
+        },
+        line2: {
+            type: String,
+        },
+        city: {
+            type: String,
+            required: true
+        },
+        county: {
+            type: String,
+        },
+        zip: {
+            type: Number,
+        }
+}
+}, {
+    collection: 'primaryData',
+    timestamps: true
+});
+
+
+
+//collection for eventData includes embedded address object schema
+let eventDataSchema = new Schema({
+    _id: { type: String, default: uuid.v1 },
+    event_id: {
+        type: Number,
+        require: true,
+        unique: true
+
+    },
+
+    eventName: {
+        type: String,
+        require: true
+    },
+    services: [{
+        type: String,
+        require: true
+    }],
+    date: {
+        type: Date,
+        required: true
+    },
+    address: {
+        line1: {
+            type: String
+        },
+        line2: {
+            type: String,
+        },
+        city: {
+            type: String,
+        },
+        county: {
+            type: String,
+        },
+        zip: {
+            type: Number,
+        }
+    },
+    description: {
+        type: String,
+    },
+    // acts as the list of attendees 
+    attendees: [{
+        type: Number
+    }]
+}, {
+    collection: 'eventData'
+});
+
+
+
+
+
+
+
+// create models from mongoose schemas
+const primarydata = mongoose.model('primaryData', primaryDataSchema);
+const eventdata = mongoose.model('eventData', eventDataSchema);
+// const clientaddress = mongoose.model('cilentaddress',cilentaddressSchema)
+// package the models in an object to export 
+module.exports = { primarydata, eventdata }
---
+++ backend-B/routes/primaryData.js
@@ -0,0 +1,234 @@
+const express = require("express"); 
+const router = express.Router(); 
+
+//importing data model schemas
+let { primarydata } = require("../models/models"); 
+let { eventdata } = require("../models/models"); 
+
+
+//  https://dirask.com/posts/JavaScript-subtract-months-from-date-pVmgGD#:~:text=In%20this%20article%2C%20we%20would%20like%20to%20show,2%29%3B%20%2F%2F%20subtracted%202%20months%20from%20existing%20date
+const subtractMonths = (date, months) => {
+    const result = new Date(date);
+    result.setMonth(result.getMonth() - months);
+    return result;
+  };
+
+
+//GET all entries
+router.get("/", (req, res, next) => { 
+    primarydata.find( 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
+});
+
+//GET single entry by ID
+router.get("/id/:id", (req, res, next) => {
+    primarydata.find( 
+        { _id: req.params.id }, 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//GET entries based on search query
+//Ex: '...?firstName=Bob&lastName=&searchBy=name' 
+router.get("/search/", (req, res, next) => { 
+    let dbQuery = "";
+    if (req.query["searchBy"] === 'name') {
+        dbQuery = { firstName: { $regex: `^${req.query["firstName"]}`, $options: "i" }, lastName: { $regex: `^${req.query["lastName"]}`, $options: "i" } }
+    } else if (req.query["searchBy"] === 'number') {
+        dbQuery = {
+            "phoneNumbers.primaryPhone": { $regex: `^${req.query["phoneNumbers.primaryPhone"]}`, $options: "i" }
+        }
+    };
+    primarydata.find( 
+        dbQuery, 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//GET clients off of their number
+// http://localhost:3000/primaryData/getnum/8329412894
+router.get("/getnum/:nums", (req, res, next) => { 
+    let dbQuery = "";
+    dbQuery = { phoneNumbers: { "$all" : req.params.nums} } 
+    // console.log(req.params.nums)
+    // console.log(dbQuery)
+    primarydata.find(dbQuery , 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//GET events for a single client
+router.get("/events/:id", (req, res, next) => { 
+    eventdata.find(
+    { attendees: req.params.id },
+    (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.json(data);
+        }
+    })}
+);
+
+//POST
+router.post("/", (req, res, next) => { 
+    primarydata.create( 
+        req.body,
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data); 
+            }
+        }
+    );
+    primarydata.createdAt;
+    primarydata.updatedAt;
+    primarydata.createdAt instanceof Date;
+});
+
+//PUT update (make sure req body doesn't have the id)
+router.put("/:id", (req, res, next) => { 
+    primarydata.findOneAndUpdate( 
+        { _id: req.params.id }, 
+        req.body,
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//Lauren 
+//DELETE for the intake form, which remvoes a client based on the _id 
+router.delete("/:id", (req,res,next)=>{
+    primarydata.deleteOne(
+        {_id:req.params.id}, 
+        (error,data)=>{
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+
+//GET clients off of their number
+// http://localhost:3000/primaryData/getnum/8329412894
+router.get("/getnum/:nums", (req, res, next) => { 
+    let dbQuery = "";
+    dbQuery = { phoneNumbers: { "$all" : req.params.nums} } 
+    // console.log(req.params.nums)
+    // console.log(dbQuery)
+    primarydata.find(dbQuery , 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+
+
+// Lam 
+// count of cilents who signed up for events past two months
+router.get("/search_attendee_2_months/", (req,res,next)=>{
+
+    eventdata.find({
+    date: {
+        $gte: subtractMonths(new Date(), 2),
+        $lte: new Date()
+    }}
+    ,{eventName:1,attendees:1,date:1},
+    (error, data) => { 
+        if (error) {
+            return next(error);
+        } else {
+        
+            
+            // console.log(data);
+            // lam test 
+            let count = []
+            var dict = {}
+
+            // loops through the obj to grab the values within the objects
+            // dict drabs the distinct values tied to an event
+            // the array act as a counter to count the total num of attendees over all
+
+            for (const i in data) {   
+                    // count.push(x)
+                for (const [key, value] of Object.entries(data[i]["attendees"])) {
+                    // console.log(key, value);
+                    if (dict.hasOwnProperty(data[i]["eventName"])) {
+                        dict[data[i]["eventName"]].push(value)
+                    }
+                    else {
+                        dict[data[i]["eventName"]] = [value]
+                    }
+                    count.push(value)
+                    }
+              }
+
+
+            let test = []
+            // loops through my dict object and then grabs the key and values to form the string within the array test  
+            for (const [key, value] of Object.entries(dict)) {
+            test.push("The number of attendees for the" + key + " is " + (dict[key].length).toString());
+            }
+            // console.log(test)
+            // joins the string and then concatenating that string to another string
+            let output_string = (test.join());
+            let output = "This report returns the number of attendees who have signed for an event that is taking place between now and two months prior. " + output_string + ". The number of attendees that signed up for an event during the past two months is: " 
+            + (count.length).toString()  
+            res.json(output);
+        }
+    }
+)
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
