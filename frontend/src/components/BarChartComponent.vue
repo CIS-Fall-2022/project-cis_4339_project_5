aa6f68bd3dd641940eefd7cb6fb126bbd2669558
LaurenHolley <77747367+LaurenHolley@users.noreply.github.com>
2022-11-16 20:34:15 -0600

    Bar Chart Comments
    Added explanations to dashboard chart

--- frontend/src/components/BarChartComponent.vue
+++ frontend/src/components/BarChartComponent.vue
@@ -1,14 +1,18 @@
 <template>
+  <!--Included a styled header above the bar chart as a title to describe to the user what the data is referring to-->
   <h1 style="font-size:150%; text-align:center; font-family:arial;color:rgb(140,10,10)">2 Months Event Attendance</h1>
   <canvas ref="myChart" ></canvas>
 </template>
 
 <script>
+//we used chartjs library to create the bar chart for our dashboard, we made this decision because there
+//is lots of room for flexibility and it allowed us to get a simple chart running in vue
 import { Chart, registerables } from 'chart.js'
 
 Chart.register(...registerables);
 export default {
   props: {
+    //chart js requires the data to be in json format
     label: {
       type: Array,
     },
@@ -18,13 +22,18 @@ export default {
   },
   async mounted() {
     console.log(this.chartData);
+    //creating a new chart instance that is of type bar, we chose a bar chart because it clearly displays
+    //the number of attendees for each individual event and allows you to quickly compare attendance across events
     await new Chart(this.$refs.myChart, {
       type: "bar",
+      //that data of the chart is not local, but instead uses an API to retrieve data from the database,
+      //which is why we have to use the 'this' keyword to identfiy the label and data
       data: {
         labels: this.label,
         datasets: [
           {
             label: "Attendees",
+            //included a list of rgb colors to have each bar be represented in a different shade of blue
             backgroundColor: [
                "rgba(100,100,200, 0.6 )",
                "rgba(0,180,240, 0.6 )",
@@ -38,6 +47,8 @@ export default {
       },
       options: {
         plugins:{
+          //included the options for the legend so that we can adjust postion and color in order
+          //to better fit the dashboard 
           legend:{
             display:true,
             position:'bottom',
@@ -46,6 +57,7 @@ export default {
             }
           }
         },
+        //we made some adjustments to the y axis scale to better represent that attendee data
         scales: {
             y: {
               beginAtZero: true,
